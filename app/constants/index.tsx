999953b5374361af97108758ed17561f58cad81c
Bookieblue <oluwabukola.fasan@gmail.com>
2024-03-30 21:33:56 +0100

    single-startup correction

--- app/constants/index.tsx
+++ app/constants/index.tsx
@@ -1,3 +1,4 @@
+
 // COUNTRY DROPDOWN MENU
 export const AFRICAN_COUNTRIES = [
   {
@@ -6,7 +7,7 @@ export const AFRICAN_COUNTRIES = [
   },
   {
     code: 'ETH',
-    name: 'Ethiopia',
+    name: 'Ethiopia'
   },
   {
     code: 'CGO',
@@ -52,15 +53,15 @@ export const AFRICAN_COUNTRIES = [
     code: 'GHA',
     name: 'Ghana',
   },
-  {
+   {
     code: 'MOZ',
     name: 'Mozambique',
   },
-  {
+    {
     code: 'MDG',
     name: 'Madagascar',
   },
-  {
+    {
     code: 'CIV',
     name: "Côte d'Ivoire",
   },
@@ -146,7 +147,7 @@ export const AFRICAN_COUNTRIES = [
   },
   {
     code: 'LBR',
-    name: 'Liberia',
+    name:  'Liberia',
   },
   {
     code: 'MRT',
@@ -198,11 +199,11 @@ export const AFRICAN_COUNTRIES = [
   },
   {
     code: 'COM',
-    name: 'Comoros',
+    name:  'Comoros',
   },
   {
     code: 'CPV',
-    name: 'Cabo Verde',
+    name:  'Cabo Verde',
   },
   {
     code: 'STP',
@@ -221,87 +222,298 @@ export const AFRICAN_COUNTRIES = [
   name: string;
 }[];
 
+
+
 //COMPANY FIELD DROPDOWN MENU
 export const COMPANY_FIELD = [
-  {
-    name: 'E-commerce',
-  },
-  {
-    name: 'Health',
-  },
-  {
-    name: 'Banking and Finance',
-  },
-  {
-    name: 'Information and Technology',
-  },
-  {
-    name: 'Agriculture',
-  },
+     {
+      name: 'E-commerce'
+     },
+     {
+      name: 'Health'
+     },
+     {
+      name: 'Banking and Finance'
+     },
+     {
+      name: 'Information and Technology'
+     },
+     {
+      name: 'Agriculture'
+     }
 ] as {
   name: string;
 }[];
 
-// TRENDING STARTUPS SECTION
-export const TRENDING_STARTUPS = [
-  {
-    name: 'Opay',
-    icon: '/opay.svg',
-    field: 'Fintech',
-    location: 'Lagos, Nigeria',
-    description: 'Automated security compliance for your business in weeks',
-  },
-  {
-    name: 'Jumia',
-    icon: '/jumia.svg',
-    field: 'Ecommerce',
-    location: 'Lagos, Nigeria',
-    description: 'Automated security compliance for your business in weeks',
-  },
-  {
-    name: 'Eden Life',
-    icon: '/eden.svg',
-    field: 'Mobile Food',
-    location: 'Lagos, Nigeria',
-    description: 'Automated security compliance for your business in weeks',
-  },
-];
 
-// TRENDING STARTUPS SECTION
-export const STARTUPS = [
-  {
-    name: 'Opay',
-    icon: '/opay.svg',
-    field: 'Fintech',
-    location: 'Lagos, Nigeria',
-    description: 'Automated security compliance for your business in weeks',
-    supports: 78,
-  },
-  {
-    name: 'Jumia',
-    icon: '/jumia.svg',
-    field: 'Ecommerce',
-    location: 'Lagos, Nigeria',
-    description: 'Automated security compliance for your business in weeks',
-    supports: 1243,
-  },
-  {
-    name: 'M-Pesa',
-    icon: '/m-pesa.svg',
-    field: 'Fintech',
-    location: 'Nairobi, Kenya',
-    description: 'Automated security compliance for your business in weeks',
-    supports: 1150,
-  },
-  {
-    name: 'Eden Life',
-    icon: '/eden.svg',
-    field: 'Mobile Food',
-    location: 'Lagos, Nigeria',
-    description: 'Automated security compliance for your business in weeks',
-    supports: 150,
-  },
-];
+  // TRENDING STARTUPS SECTION
+  export const TRENDING_STARTUPS = [
+    {
+      name: 'Opay',
+      icon: '/opay.svg',
+      field: 'Fintech',
+      location: 'Lagos, Nigeria',
+      description:
+        'Automated security compliance for your business in weeks',
+    },
+    {
+      name: 'Jumia',
+      icon: '/jumia.svg',
+      field: 'Ecommerce',
+      location: 'Lagos, Nigeria',
+      description:
+        'Automated security compliance for your business in weeks',
+    },
+    {
+      name: 'Eden Life',
+      icon: '/eden.svg',
+      field: 'Mobile Food',
+      location: 'Lagos, Nigeria',
+      description:
+        'Automated security compliance for your business in weeks',
+    },
+
+  ];
+
+  // COUNTRIES ICON SECTION
+    export const  STARTUPS_COUNTRIES = [
+      {
+        name: 'All',
+        icon:  '/african-icon2.svg',
+        url: '/',
+      },
+      {
+        name: 'Nigeria',
+        icon: '/nigeria.svg',
+        url: '/',
+      },
+      {
+        name: 'South Africa',
+        icon:  '/south-africa.svg',
+        url: '/',
+      },
+      {
+        name: 'Kenya',
+        icon:  '/kenya.svg',
+        url: '/',
+      },
+      {
+        name: 'Eygpt',
+        icon: '/eygpt.svg',
+        url: '/',
+      },
+      {
+        name: 'Morrocco',
+        icon: '/morrocco.svg',
+        url: '/',
+      },
+      {
+        name: 'Tanzania',
+        icon: '/tanzania.svg',
+        url: '/',
+      },
+
+      {
+        name: 'Algeria',
+        icon: '/algeria.svg',
+        url: '/',
+      },
+  
+      {
+        name: 'Ghana',
+        icon: '/ghana.svg',
+        url: '/',
+      },
+  
+    ];
+
+
+    //STARTUPS SECTION
+    export const STARTUPS = [
+      {
+        name: 'Opay',
+        icon: '/opay.svg',
+        field: 'Fintech',
+        location: 'Lagos, Nigeria',
+        description:
+          'Automated security compliance for your business in weeks',
+        supports: 78
+      },
+      {
+        name: 'Jumia',
+        icon: '/jumia.svg',
+        field: 'Ecommerce',
+        location: 'Lagos, Nigeria',
+        description:
+        'Automated security compliance for your business in weeks',
+        supports: 1243
+      },
+      {
+        name: 'M-Pesa',
+        icon: '/m-pesa.svg',
+        field: 'Fintech',
+        location: 'Nairobi, Kenya',
+        description:
+          'Automated security compliance for your business in weeks',
+        supports: 1150
+      },
+      {
+        name: 'Eden Life',
+        icon: '/eden.svg',
+        field: 'Mobile Food',
+        location: 'Lagos, Nigeria',
+        description:
+          'Automated security compliance for your business in weeks',
+        supports: 150
+      },
+  
+    ];
+
+       //EXPLORE STARTUPS SECTION
+       export const EXPLORE_STARTUPS = [
+        {
+          name: 'Opay',
+          icon: '/opay.svg',
+          field: 'Fintech',
+          location: 'Lagos, Nigeria',
+          description:
+            'Automated security compliance for your business in weeks',
+          supports: 1110,
+          url: '/single-startup'
+        },
+        {
+          name: 'Jumia',
+          icon: '/jumia.svg',
+          field: 'Ecommerce',
+          location: 'Lagos, Nigeria',
+          description:
+          'Automated security compliance for your business in weeks',
+          supports: 150,
+          url: '/single-startup'
+        },
+        {
+          name: 'M-Pesa',
+          icon: '/m-pesa.svg',
+          field: 'Fintech',
+          location: 'Nairobi, Kenya',
+          description:
+            'Automated security compliance for your business in weeks',
+          supports: 110,
+          url: '/single-startup'
+        },
+        {
+          name: 'Eden Life',
+          icon: '/eden.svg',
+          field: 'Mobile Food',
+          location: 'Lagos, Nigeria',
+          description:
+            'Automated security compliance for your business in weeks',
+          supports: 200,
+          url: '/single-startup'
+        },
+        {
+          name: 'Alt School',
+          icon: '/altschool.svg',
+          field: 'Tech School',
+          location: 'Lagos, Nigeria',
+          description:
+            'Automated security compliance for your business in weeks',
+          supports: 9,
+          url: '/single-startup'
+        },
+        {
+          name: 'Paystack',
+          icon: '/paystack.svg',
+          field: 'Fintech',
+          location: 'Lagos, Nigeria',
+          description:
+            'Automated security compliance for your business in weeks',
+          supports: 2,
+          url: '/single-startup'
+        },
+    
+      ];
+
+
+      //EXPLORE TRENDING COMPANIES  SECTION
+      export const TRENDING_COMPANIES = [
+        {
+          name: 'Paystack',
+          icon: '/paystack.svg',
+          description:
+            'We’re constantly using, reviewing and comparing all tools ',
+          url: ''
+        },
+        {
+          name: 'Flutterwave',
+          icon: '/flutterwave.svg',
+          description:
+          'We’re constantly using, reviewing and comparing all tools',
+          url: ''
+        },
+        {
+          name: 'Allawee',
+          icon: '/allawee.svg',
+          description:
+            'We’re constantly using, reviewing and comparing all tools',
+          url: ''
+        },
+        {
+          name: 'BrassHQ',
+          icon: '/brasshq.svg',
+          description:
+            'We’re constantly using, reviewing and comparing all tools',
+          url: ''
+        },
+    
+      ];
+  
+
+    //FOOTER SOCIAL LINK SECTION
+    export const SOCIAL_LINKS = [
+
+      {
+        icon: '/instagram.svg',
+        url: '/'
+      },
+      {
+        icon: '/twitter.svg',
+        url: '/'
+      },
+      {
+        icon: '/linkedin.svg',
+        url: '/'
+      },
+      {
+        icon: '/facebook.svg',
+        url: '/'
+      }
+
+  ]
+
+    export const FOOTER_INFO_LINKS = [
+
+      {
+        title: 'Submit Startup',
+        url: '/submit-startup'
+      },
+      {
+        title: 'Suggest Startup',
+        url: '/suggest-startup'
+      },
+      {
+        title: 'Feedback',
+        url: '/Feedback'
+      }
+
+  ]
+
+
+  export const FOOTER_LINKS = [
+        'Contact Us',
+        'Terms of Use',
+        'Privacy Policy',
+  ]
 
 // SINGLE STARTUP social icons
 export const SOCIALICONS = [
